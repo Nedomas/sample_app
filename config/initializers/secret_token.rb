eb2d6ba202b63de268822bcfc1992108627e180a
Nedomas <domas.bitvinskas@me.com>
2012-07-21 20:46:49 +0300

    Initial seed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '48a40279730516128aef3127c868a3638c16cd80e7ce252e0d9f919edd6e28175212503a590734187690881de83af81abfe7b38c794c05d7f3ea4d20463f71e8'
